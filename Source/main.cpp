c3d58c0a28f5ba0b8d512ef40b6c61e3af1be23a
UnknownNeon <twidlestick@gmail.com>
2024-01-09 20:42:58 +0530

    Rendering a blue background and the bricks on screen ! #learning

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,42 +1,118 @@
 //The raylib library must be installed to run raylib code
+#include <vector>
+#include <iostream>
 #include "raylib.h"
+
+void UpdateGame();
+void DrawGame();
+void gameStartup();
+
+struct Ball {
+    Vector2 pos;
+    Vector2 acceleration{ 1.0f, 1.0f };
+    float velocity;
+    float radius = 5.0f;
+};
+
+struct Brick {
+    Rectangle rect;
+    Color color;
+    float w = 50.0f;
+    float h = 20.0f;
+};
+
+struct Player {
+        Rectangle rect;
+        float velocity = 250.0f;
+        int score = 0;
+        float w = 75.0f;
+        float h = 10.0f;
+    }; // This is the struct for the player i,e the bar
+
+
+Player player;
+Ball ball;
+
+std::vector<Brick> bricks;
 
     int main(void)
     {
-        // Initialization
-        //--------------------------------------------------------------------------------------
-        const int screenWidth = 800;
-        const int screenHeight = 450;
+        const int screenWidth = 500;
+        const int screenHeight = 600;
 
-        InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+        InitWindow(screenWidth, screenHeight, "brick breaker game");
+        SetTargetFPS(60); 
 
-        SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-        //--------------------------------------------------------------------------------------
+        gameStartup();
 
-        // Main game loop
         while (!WindowShouldClose())    // Detect window close button or ESC key
         {
             // Update
-            //----------------------------------------------------------------------------------
-            // TODO: Update your variables here
-            //----------------------------------------------------------------------------------
+            void UpdateGame();
 
             // Draw
-            //----------------------------------------------------------------------------------
             BeginDrawing();
 
-            ClearBackground(RAYWHITE);
-
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            ClearBackground(BLUE);
+            DrawGame();
 
             EndDrawing();
-            //----------------------------------------------------------------------------------
         }
 
-        // De-Initialization
-        //--------------------------------------------------------------------------------------
-        CloseWindow();        // Close window and OpenGL context
-        //--------------------------------------------------------------------------------------
+        CloseWindow();        
 
         return 0;
+    }
+
+    void gameStartup() {
+        
+        //Loading the player Presets
+        player.rect = Rectangle{ 250.0f, 540.0f, player.w, player.h };
+        player.score = 0;
+
+        // loading the ball data
+        ball.pos = Vector2{ 300, 300 };
+        ball.velocity = 300.0f;
+
+        Brick newBrick;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                newBrick.rect = Rectangle{
+                    float(40 + (i * 55)),
+                    float(50 + (j * 26)),
+                    newBrick.w,
+                    newBrick.h
+                };
+                newBrick.color = RED;
+                bricks.push_back(newBrick);
+            }
+        }
+    }
+
+    void UpdateGame() {
+        float frameTime = GetFrameTime();
+
+        if (IsKeyDown(KEY_A)) {
+            player.rect.x += player.velocity * frameTime;
+        }
+        if (IsKeyDown(KEY_D)) {
+            player.rect.x -= player.velocity * frameTime;
+        }
+    }
+
+    void DrawGame() {
+
+        Brick brick;
+        for (int i = 0; i < bricks.size(); i++) {
+            brick = bricks[i];
+            DrawRectangle(
+                brick.rect.x,
+                brick.rect.y,
+                brick.rect.width,
+                brick.rect.height,
+                brick.color
+            );
+        }
+
+        DrawCircle(ball.pos.x, ball.pos.y, ball.radius, RAYWHITE);
     }
